1f48fecc76a1609885aa0f9c9118a0b79a1158fe
Jordan White <jw6688187@gmail.com>
2026-01-24 14:26:57 -0600

    renamed file

--- test.py
+++
@@ -1,6 +0,0 @@
-from point_cloud import PointCloud
-
-image_path = "test.jpg"
-
-pc = PointCloud(image_path="inputs/" + image_path)
-pc.write_ply(destination="outputs/")
---
+++ usage.py
@@ -0,0 +1,16 @@
+from point_cloud import PointCloud
+
+image_path = "test.jpg"
+
+# example models to test
+models_to_test = [
+    "Intel/dpt-swinv2-tiny-256",
+    "Intel/dpt-hybrid-midas",
+    "LiheYoung/depth-anything-small-hf",
+    "depth-anything/Depth-Anything-V2-Small-hf",
+]
+
+for model_name in models_to_test:
+    print(f"Generating point cloud for model: {model_name}")
+    pc = PointCloud(image_path="inputs/" + image_path, model=model_name)
+    pc.write_ply(destination="outputs/")
